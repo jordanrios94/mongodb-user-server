09a864ea2491a9bdf9d11efd8aaf5ebc9dd7afc0
jordanrios94 <jordanrios.software@gmail.com>
2018-12-28 14:07:04 +0000

    test: added update operator for post count

--- src/user.js
+++ src/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({ name: String });
+const UserSchema = new Schema({
+  name: String,
+  postCount: Number
+});
 
 const User = mongoose.model('user', UserSchema);
 
